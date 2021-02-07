1b4ccd40722095692b5d15ab51df40e8b3c8a0b8
lix.digito <lix.digito@hotmail.com>
2021-02-06 21:57:08 -0300

    buscar na api

--- src/pages/Home/UseEffect/AsyncWait/index.js
+++ src/pages/Home/UseEffect/AsyncWait/index.js
@@ -1,21 +1,39 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {GetUserGit} from '../../../../Repository/ApiGit';
 
 export default function AsyncAwait() {
+  const [urlAvatar, setUrlAvatar] = useState('');
+  const [inputName, setInputName] = useState('');
   
   async function callAsync(name) {
-    const response = await GetUserGit(name);
-    console.log(response);
+    try {
+      const response = await GetUserGit(name);    
+      console.log(response);
+      const url = response.data.avatar_url;
+      setUrlAvatar(url);  
+    } catch {
+      alert('Erro ao buscar os dados.');
+    }    
   }
 
 
   useEffect(() => {
-    callAsync('react');
-  },[]);
+    if (inputName) {
+      callAsync(inputName);
+    }    
+  },[inputName]);
 
   return (
     <div>
-      <h3>Componente AsyncWait</h3>      
+      <h3>Componente AsyncWait</h3> 
+      <div>
+        <input
+          type="text" 
+          placeholder="Nome:"
+          onBlur={e => setInputName(e.target.value)}
+        />
+      </div>     
+      <img src={urlAvatar} />     
     </div>
   )
 
